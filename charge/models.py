52e58b52db05eda663a3d197e6e05b3fc9d7cfdf
Cristian Blanco <cristiamblancogarcia@gmail.com>
2019-04-25 14:02:44 -0600

    Cree el modelo cargo

---
+++ charge/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from employee.models import Employee
+
+
+class Charge(models.Model):
+    description = models.CharField(max_length=20, verbose_name=_('Description'), unique=True)
+    employee = models.ManyToManyField(Employee, verbose_name=_('Employee'))
+
+    def __str__(self):
+        return '{}'.format(self.description, self.employee)
+
+    class Meta:
+        verbose_name_plural =_('Charges')
+        verbose_name = _('Charge')
+
+
