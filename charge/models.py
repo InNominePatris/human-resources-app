25980623e87ae9ced885378c210f66819debd0d8
Cristian Blanco <cristiamblancogarcia@gmail.com>
2019-05-01 12:37:00 -0600

    Eliminé relación con empleado

--- charge/models.py
+++ charge/models.py
@@ -5,10 +5,9 @@ from employee.models import Employee
 
 class Charge(models.Model):
     description = models.CharField(max_length=20, verbose_name=_('Description'), unique=True)
-    employee = models.ManyToManyField(Employee, verbose_name=_('Employee'))
 
     def __str__(self):
-        return '{}'.format(self.description, self.employee)
+        return '{}'.format(self.description)
 
     class Meta:
         verbose_name_plural = _('Charges')
