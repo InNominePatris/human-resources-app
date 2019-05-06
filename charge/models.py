15137c62754d9c4e2d282b5eee3123975b5f67af
Cristian Blanco <cristiamblancogarcia@gmail.com>
2019-05-06 17:44:05 -0600

    resolvi conflictos con espacios

--- charge/models.py
+++ charge/models.py
@@ -13,19 +13,19 @@ class Charge(models.Model):
         verbose_name_plural = _('Charges')
         verbose_name = _('Charge')
 
+
 class History(models.Model):
-    detail= models.CharField(max_length=20, verbose_name=_('Detail'))
-    initial_date= models.DateField(verbose_name=_('Initial date'))
+    detail = models.CharField(max_length=20, verbose_name=_('Detail'))
+    initial_date = models.DateField(verbose_name=_('Initial date'))
     final_date = models.DateField(verbose_name=_('Final date'))
     employee = models.ForeignKey(Employee, on_delete= models.CASCADE, verbose_name=_('Employee'))
     charge = models.ForeignKey(Charge, on_delete= models.CASCADE,verbose_name=_('charge'))
-    
 
     def __str__(self):
-        return '{} {} {} {} {}'.format(self.detail.initial_date.final_date.employee.charge)
+        return '{} {} {} {} {} {}'.format(self.detail.initial_date.final_date.employee.charge)
     
     class Meta:
-        verbose_name_plural= _('Histories')
-        verbose_name= _('History')
+        verbose_name_plural = _('Histories')
+        verbose_name = _('History')
     
 
