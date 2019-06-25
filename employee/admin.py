dd175f1c3873d817f021c73eb1df1579bdd46c0e
Cristian Blanco <cristiamblancogarcia@gmail.com>
2019-06-24 19:57:53 -0600

    Edición de tabla empleado y creación de tabla nivel de escolaridad

--- employee/admin.py
+++ employee/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Employee
-from django.contrib.auth.models import User, Group
+from .models import Employee, AcademicStudies
 
 
-admin.site.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ['first_name']
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(AcademicStudies)
+
 
--- employee/models.py
+++ employee/models.py
@@ -22,7 +22,6 @@ class Employee(models.Model):
         (STATUS_MARRIED, _('Married')),
         (STATUS_DIVORCED, _('Divorced'))
     )
-
     first_name = models.CharField(max_length=20, verbose_name=_('First name'))
     last_name = models.CharField(max_length=20, verbose_name=_('Last name'))
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name=_('Gender'))
@@ -31,8 +30,8 @@ class Employee(models.Model):
     civil_status = models.CharField(max_length=2, choices=STATUS_CHOICES, verbose_name=_('Civil status'))
     address = models.CharField(max_length=50, verbose_name=_('Address'))
     phone = PhoneNumberField(verbose_name=_('Phone'))
-    naturalized = models.BooleanField(default=False, verbose_name=_('Naturalized'))
-
+    email = models.EmailField(max_length=70, blank=False, default=False, verbose_name=_('Email'))
+    nationality = models.CharField(max_length=20, verbose_name=_('Nationality'), blank=True)
     history = HistoricalRecords()
 
     def __str__(self):
@@ -43,4 +42,59 @@ class Employee(models.Model):
         verbose_name = _('Employee')
 
 
+class AcademicStudies(models.Model):
+    PRIMARY_SCHOOL_CHOICE = 'Primary school'
+    HIGH_SCHOOL_CHOICE = 'High school'
+    UNIVERSITY_CHOICE = 'University'
+    BACHELOR_DEGREE = 'Bachelor degree'
+    MASTER_DEGREE = 'Master degree'
+    DOCTOR_DEGREE = 'Doctor degree'
+    POST_DOCTOR_DEGREE = 'Post doctor degree'
+    PROFESSIONAL_SCHOOL = 'Professional school'
+    NO_STUDY = 'No study'
 
+    EDUCATIONAL_LEVEL_CHOICES = (
+        (PRIMARY_SCHOOL_CHOICE, _('Primary')),
+        (HIGH_SCHOOL_CHOICE, _('High school')),
+        (UNIVERSITY_CHOICE, _('University')),
+        (BACHELOR_DEGREE, _('Bachelor degree')),
+        (MASTER_DEGREE, _('Master degree')),
+        (DOCTOR_DEGREE, _('Doctor degree')),
+        (POST_DOCTOR_DEGREE, _('Post doctor degree')),
+        (PROFESSIONAL_SCHOOL, _('Professional school')),
+        (NO_STUDY, _('No study'))
+    )
+
+    GRADE_BS = 'B.S'
+    GRADE_BA = 'B.A'
+    GRADE_MS = 'M.S'
+    GRADE_MA = 'M.A'
+    GRADE_PHD = 'Ph.D.'
+    GRADE_MBA = 'MBA'
+    GRADE_JD = 'JD'
+    GRADE_MD = 'MD'
+
+    GRADE_CHOICES = (
+        (GRADE_BS, _('B.S')),
+        (GRADE_BA, _('B.A')),
+        (GRADE_MS, _('M.S')),
+        (GRADE_MA, _('M.A')),
+        (GRADE_PHD, _('Ph.D.')),
+        (GRADE_MBA, _('MBA')),
+        (GRADE_JD, _('JD')),
+        (GRADE_MD, _('MD'))
+    )
+
+    employee = models.ForeignKey(
+        Employee,
+        verbose_name=_('Employee'), on_delete=models.CASCADE
+    )
+    study_type = models.CharField(max_length=20, choices=EDUCATIONAL_LEVEL_CHOICES, verbose_name=_('Study type'))
+    grade = models.CharField(max_length=6, choices=GRADE_CHOICES, verbose_name=_('Grade'))
+    description = models.CharField(max_length=200, default=False, blank=True, verbose_name=_('Description'))
+    from_date = models.DateField(null=False, verbose_name=_('From date'))
+    to_date = models.DateField(null=True, verbose_name=_('To date'))
+    institute = models.CharField(max_length=30, default=False, verbose_name=_('Institute'))
+
+    def __str__(self):
+        return '{}'.format(self.employee)
