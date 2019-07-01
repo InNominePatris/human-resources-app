3a1ea76c38363e12557939068acdaec5023b5b05
Cristian Blanco <cristiamblancogarcia@gmail.com>
2019-07-01 05:53:48 -0600

    Traducciones y clase Meta

--- benefits/apps.py
+++ benefits/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class BenefitsConfig(AppConfig):
     name = 'benefits'
+
+    verbose_name = _('Beneficios')
--- benefits/models.py
+++ benefits/models.py
@@ -13,7 +13,14 @@ class VacationRequest(models.Model):
     )
 
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name=_('Employee'))
-    date = models.DateField(verbose_name=_('Date'))
+    date = models.DateTimeField(verbose_name=_('Date'))
     detail = models.CharField(max_length=300, verbose_name=_('Detail'))
     status = models.CharField(max_length=10, choices=REQUEST_STATUS, verbose_name=_('Status'))
+
+    def __str__(self):
+        return '{} {}'.format(self.employee, self.date)
+
+    class Meta:
+        verbose_name_plural = _('Solicitudes de vacaciones')
+        verbose_name = _('Solicitud de vacaciones')
 
